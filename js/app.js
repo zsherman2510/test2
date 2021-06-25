5769d42853dbbed4ae3e964db95fa424b8b5c323
zavion sherman <zsherman2510@hotmail.com>
2021-06-24 23:34:54 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var hamburger = document.getElementById("hamburger");
+
+var menuMobile = document.getElementById("menu_mobile");
+
+hamburger.addEventListener("click", function () {
+  menuMobile.classList.contains("menu_show")
+    ? menuMobile.classList.remove("menu_show")
+    : menuMobile.classList.add("menu_show");
+});
